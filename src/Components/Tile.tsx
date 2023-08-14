d5f732698d7edfb6f492a37d5130bac9d94dfa6b
jotyot <104177975+jotyot@users.noreply.github.com>
2023-08-14 09:27:01 -0700

    pack

--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -10,20 +10,19 @@ function Tile({ text, id, flippedProps }: Props) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: id,
   });
-  const style = transform
-    ? { transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` }
-    : undefined;
-  // : {
-  //     transitionProperty: "all",
-  //     transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-  //     transitionDuration: "150ms",
-  //   };
+  const style = {
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : "none",
+  };
 
   return (
     <button
-      className=" aspect-square w-24 rounded-lg select-none
+      className={` aspect-square w-24 rounded-lg select-none
       bg-zinc-100 border-b-4 border-zinc-300 
-      flex justify-center items-center text-center "
+      flex justify-center items-center text-center ${
+        !flippedProps && !transform ? "transition-transform" : ""
+      }`}
       ref={setNodeRef}
       style={style}
       {...listeners}
