1540e1702f8cf0dd9fd84a1361f950be595a5403
jotyot <104177975+jotyot@users.noreply.github.com>
2023-08-15 04:36:11 -0700

    fonts

--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -5,13 +5,16 @@ interface Props {
 }
 
 function Tile({ text, flippedProps, isDragging = false }: Props) {
+  const long = text.length > 30;
+  const med = text.length > 15;
   return (
     <div
       className={` aspect-square rounded-lg select-none
       bg-zinc-100 border-b-4 border-zinc-300 
-      absolute flex justify-center items-center text-center 
-      shadow-md
-      ${isDragging ? "w-32 text-lg" : "w-24 text-sm"}`}
+      absolute flex justify-center items-center w-24
+      text-center shadow-md font-sans p-1 font-medium
+      ${isDragging ? " scale-150" : " scale-100 "} 
+      ${long ? "text-xs" : med ? "text-sm" : "text-md"}`}
       {...flippedProps}
     >
       {text}
