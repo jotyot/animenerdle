102dbb6e52222b2d746cab2829cf4e32332cc9e7
jotyot <104177975+jotyot@users.noreply.github.com>
2023-11-12 23:17:25 -0800

    omg useeffect my beloved

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
 import Grid from "./Components/Grid";
 import { db } from "./firebase";
 import { getDoc, doc } from "firebase/firestore";
+import { PuzzleTemplate } from "./Classes/Puzzle";
 
-async function getPuzzle() {
-  const docSnap = await getDoc(doc(db, "puzzles", "current"));
-  const puzzle = {
-    properties: docSnap.get("properties"),
-    entries: docSnap.get("entries"),
-  };
-  return puzzle;
-}
+function App() {
+  const [puzzleTemplate, setPuzzleTemplate] = useState<PuzzleTemplate>({
+    properties: [],
+    entries: [],
+  });
+  const [loading, setLoading] = useState(true);
 
-async function App() {
-  const puzzle = await getPuzzle();
-  return <Grid puzzleTemplate={puzzle} />;
+  useEffect(() => {
+    async function getPuzzle() {
+      const docSnap = await getDoc(doc(db, "puzzles", "current"));
+      const puzzle = {
+        properties: docSnap.get("properties"),
+        entries: docSnap.get("entries"),
+      };
+      setPuzzleTemplate(puzzle);
+      setLoading(false);
+    }
+
+    getPuzzle();
+  }, []);
+
+  return loading ? <p>Loading...</p> : <Grid puzzleTemplate={puzzleTemplate} />;
 }
 
 export default App;
